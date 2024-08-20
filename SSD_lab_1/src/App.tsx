02fa531a4a85691b953dd2a45e7c7d353ce0c3d2
anasAnonymous <k214761@nu.edu.pk>
2024-08-20 14:03:12 +0500

    SSS-lab-1_First_time

---
+++ SSD_lab_1/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>SSD Lab 1</h1>
+      <div className="card">
+      <h3 className="read-the-docs">
+      The following are the tasks for this lab. </h3>
+      <p> Creation of Git account( if you already have, then no need).</p>
+      <p> Pushing of project folder on online git by creating a local
+repository.</p>
+      <p> Pulling of the repository from git after the changes have been
+done.</p>
+      <p> Version control using private repositories.</p>
+      <p> Branching and merging the branches.</p>
+      <p> Private and public repositories.</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
